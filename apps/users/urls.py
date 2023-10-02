c9f5cf6cf16e8d2df97bd2ca0c881ee2ee45bc8a
NicoDS9688 <nicolasdickel@hotmail.com>
2023-10-02 20:45:18 -0300

    User y Register

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,14 @@
-from apps.users.views import login
+""" Vistas Users. """
+from apps.users.views import login_user, logout_user, register_user
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import messages
 from django.urls import path
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login_user', login_user, name='login'),
+    path('logout_user', logout_user, name='logout'),
+    path('register_user', register_user, name='register'),
 ]
 
 if settings.DEBUG:
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.core.urls")),
+    path("users/", include ('django.contrib.auth.urls')),
     path("users/", include("apps.users.urls")),
 
 ]
