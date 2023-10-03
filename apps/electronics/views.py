cb5c0389c7a5bb1191751d04b4767ada3eed5259
NicoDS9688 <nicolasdickel@hotmail.com>
2023-10-03 18:47:13 -0300

    mas forms

--- apps/electronics/views.py
+++ apps/electronics/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+""" Vistas Electronics"""
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from .models import Electronic, ElectronicCategory
+from .forms import ElectronicForm
 
 # Create your views here.
+def electronic_add(request):
+    """ Vista para electrónicos. """
+    if request.method == 'POST':
+        form = ElectronicForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data  # Obtiene los datos válidos del formulario
+            images = request.FILES.getlist('images')
+
+            # Crea un objeto Motor utilizando los datos válidos
+            new_electronic = Electronic.objects.create(
+                name=data['name'],
+                description=data['description'],
+                location=data['location'],
+                price=data['price'],
+            )
+
+            return redirect(reverse('electronic_add'))
+
+    else:
+        form = ElectronicForm()
+
+    context = {
+        'categories': ElectronicCategory.objects.all(),
+        'form': form,
+    }
+
+    return render(request, 'electronics/electronic_form.html', context)
--- apps/furnitures/urls.py
+++ apps/furnitures/urls.py
@@ -1,11 +1,11 @@
-from apps.electronics.views import electronic_add
+from apps.furnitures.views import furniture_add
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import messages
 from django.urls import path
 
 urlpatterns = [
-    path('agregar/', electronic_add, name='electronic_add'),
+    path('agregar/', furniture_add, name='furniture_add'),
 ]
 
 if settings.DEBUG:
