4f86ec0f26e6e442efdd3e2cc6398a230f3e806d
Jose <jose.weber97@hotmail.com>
2023-10-09 17:34:58 -0300

    edit y delete terminado

--- apps/furnitures/views.py
+++ apps/furnitures/views.py
@@ -65,6 +65,65 @@ def furnitures_list(request, id):
     return render(request, 'furnitures/list.html', context)
 
 
+def edit_furniture(request, id):
+
+    if request.user.is_authenticated:
+
+        my_furniture = Furniture.objects.get(id=id)
+
+        if request.method == 'POST':
+            form = FurnitureForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+
+                category_id = request.POST.get('category_id')
+                category = FurnitureCategory.objects.get(id=category_id)
+
+                my_furniture.title = data['title']
+                my_furniture.price = data['price']
+                my_furniture.location = data['location']
+                my_furniture.phone_number1 = data['phone1']
+                my_furniture.phone_number2 = data['phone2']
+                my_furniture.email = data['email']
+                my_furniture.description = data['description']
+                my_furniture.category = category
+
+                my_furniture.save()
+
+                messages.success(request, 'Oferta de Mobiliario editada')
+                return redirect(reverse('inicio'))
+
+            else:
+
+                print("Formulario no valido")
+                print(form.errors)
+                return redirect(reverse('inicio'))
+
+        else:
+
+            data = {
+                'title': my_furniture.title,
+                'price': my_furniture.price,
+                'location': my_furniture.location,
+                'phone1': my_furniture.phone_number1,
+                'phone2': my_furniture. phone_number2,
+                'email': my_furniture.email,
+                'description': my_furniture.description,
+            }
+
+            form = FurnitureForm(initial=data)
+
+            return render(request, 'furnitures/edit.html',
+                          {'form': form,
+                           'my_furniture': my_furniture,
+                           'categories': FurnitureCategory.objects.all()}
+                          )
+
+    else:
+        messages.error(request, '¡Usuario no autenticado!')
+        return redirect(reverse('inicio'))
+
+
 def delete_furniture(request, id):
 
     print(f'Mobiliario con id: "{id}" eliminado')
